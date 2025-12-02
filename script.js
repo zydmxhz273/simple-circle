df92d3fb29d889f2a8b02c9345eea4e2a3f03e4c
Kailai <kailai2006@gmail.com>
2025-12-02 17:52:35 +0100

    feat: now the number inside the circle wont reset

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const circle = document.querySelector('.circle');
-let count=0;
+let count=parseInt(localStorage.getItem('myScore'))||0;
+circle.innerText=count;
 circle.addEventListener('click', ()=>{
     count++;
     circle.innerText=count;
+    localStorage.setItem('myScore', count);
 });
